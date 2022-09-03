3b5aa7a581b2e735848d696d531ad538cdda88ae
iosif <solomoniosif@gmail.com>
2022-09-03 08:56:50 +0300

    changed prefix 'recipe' to 'recipes' in router settings

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -9,7 +9,7 @@ from rest_framework.routers import DefaultRouter
 from recipe import views
 
 router = DefaultRouter()
-router.register('recipe', views.RecipeViewSet)
+router.register('recipes', views.RecipeViewSet)
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
 
