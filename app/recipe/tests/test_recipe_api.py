158705000a0ea8d08d6cc3b304d9e04f913466ef
iosif <solomoniosif@gmail.com>
2022-09-03 10:11:31 +0300

    TDD: added 2 tests for an unimplemented feature: filtering recipes by tags, ingredients

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -62,7 +62,7 @@ class PublicRecipeAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class PrivaterecipeAPITests(TestCase):
+class PrivateRecipeAPITests(TestCase):
     """Test authenticated API requests"""
 
     def setUp(self):
@@ -352,6 +352,52 @@ class PrivaterecipeAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.ingredients.count(), 0)
+
+    def test_filter_by_tags(self):
+        """Test filtering recipes by tags"""
+        recipe1 = create_recipe(user=self.user, title='Cauliflower Soup')
+        recipe2 = create_recipe(user=self.user, title='Vegan Lasagna')
+        tag1 = Tag.objects.create(user=self.user, name='Vegan')
+        tag2 = Tag.objects.create(user=self.user, name='Vegetarian')
+        recipe1.tags.add(tag1)
+        recipe2.tags.add(tag2)
+        recipe3 = create_recipe(user=self.user, title='Fish and chips')
+
+        params = {'tags': f"{tag1.id},{tag2.id}"}
+        res = self.client.get(RECIPES_URL, params)
+
+        s1 = RecipeSerializer(recipe1)
+        s2 = RecipeSerializer(recipe2)
+        s3 = RecipeSerializer(recipe3)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
+    def test_filter_by_ingredients(self):
+        """Test filtering recipes by ingrediants"""
+        recipe1 = create_recipe(user=self.user, title='Mac-n-Cheese')
+        recipe2 = create_recipe(user=self.user, title='Chili con carne')
+        ingredient1 = Ingredient.objects.create(user=self.user, name='Cheddar')
+        ingredient2 = Ingredient.objects.create(
+            user=self.user, name='Chili peppers')
+        recipe1.ingredients.add(ingredient1)
+        recipe2.ingredients.add(ingredient2)
+
+        recipe3 = create_recipe(user=self.user, title='Red Lentil Curry')
+
+        params = {'ingredients': f"{ingredient1.id},{ingredient2.id}"}
+        res = self.client.get(RECIPES_URL, params)
+
+        s1 = RecipeSerializer(recipe1)
+        s2 = RecipeSerializer(recipe2)
+        s3 = RecipeSerializer(recipe3)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
 
 
 class ImageUploadTests(TestCase):
