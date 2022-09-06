11aa13d3c65b9fdf70fc62b15287c87817c36983
iosif <solomoniosif@gmail.com>
2022-09-06 04:29:41 +0300

    small reformatting for black complince

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,7 +21,10 @@ class ModelTests(TestCase):
         """Test creating a user with email is succesfull"""
         email = 'test@example.com'
         password = 'testpass123'
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
